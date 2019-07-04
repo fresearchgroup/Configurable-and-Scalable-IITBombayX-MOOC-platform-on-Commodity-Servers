fd97b2da407ff3111e1f627174d82ec217ab6038
Soumyakant Mohakul <48150731+msoumyakant14@users.noreply.github.com>
2019-07-04 15:48:05 +0530

    Update provision.sh

--- kube-provision/provision.sh
+++ kube-provision/provision.sh
@@ -1,2 +1,6 @@
+set -x
+set -o pipefail
+set -e
 kubectl exec -i $(kubectl get pods | grep mysql | cut -d ' ' -f 1) mysql mysql < provision.sql
 kubectl exec -i $(kubectl get pods | grep mongo | cut -d ' ' -f 1) mongo < mongo-provision.js
+./load-db.sh
